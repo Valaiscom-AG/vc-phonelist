b0ea2d150d8759c235918c89947d0c8a9451b8b8
Tim Luginbühl <TL@VALAISCOM.intra>
2024-08-13 08:37:34 +0200

    added Customization and Links

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const { createClient } = await import('https://cdn.skypack.dev/@supabase/supabase-js');
     const supabaseUrl = 'https://ilmufbxfsvyhpaqwdyxg.supabase.co';
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsbXVmYnhmc3Z5aHBhcXdkeXhnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxODI1OTc3NywiZXhwIjoyMDMzODM1Nzc3fQ.wdL26ds_JBVuEl_6e8TBQxRxa1Pqz2JmLQOlARKHJdE';
-  
+
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     // Function to fetch and render employees
@@ -28,16 +28,31 @@ document.addEventListener('DOMContentLoaded', async () => {
         const employeeTableBody = document.getElementById('phone-table');
         employeeTableBody.innerHTML = '';
 
+        let currentDepartment = '';
+
         data.forEach(employee => {
             console.log('Employee data:', employee); // Log each employee's data
+
+            // If the department changes, insert a new header row
+            if (employee.department !== currentDepartment) {
+                currentDepartment = employee.department;
+
+                // Create a department header row
+                const departmentHeaderRow = document.createElement('tr');
+                departmentHeaderRow.classList.add('table-danger'); // Optional: Add a class for styling the department header
+                departmentHeaderRow.innerHTML = `
+                    <td colspan="4" class="fw-bold">${currentDepartment}</td>
+                `;
+                employeeTableBody.appendChild(departmentHeaderRow);
+            }
+
+            // Create the employee row
             const row = document.createElement('tr');
             row.innerHTML = `
                 <td class="pt-3">${employee.firstname} ${employee.lastname}</td>
                 <td class="pt-3">${employee.intphone}</td>
-                <td class="pt-3">${employee.extphone}</td>
-                <td class="pt-3"><div class="collapse" id="collapseExample">
-                ${employee.mobphone}
-              </div></td>
+                <td class="pt-3"><a href="tel:${employee.extphone}">${employee.extphone}</a></td>
+                <td class="pt-3"><div class="collapse" id="collapseExample"><a href="tel:${employee.mobphone}">${employee.mobphone}</a></div></td>
                 <td class="pt-3 d-none">${employee.department}</td>
             `;
             employeeTableBody.appendChild(row);
